ec04412454a1b1a8b3059011a0bc8b469f95c7de
Zimin0 <you@example.com>
2023-10-16 22:47:02 +0300

    Creating new candy by JS instead of django. Redirect after POST added too.

--- candy/static/candy/form.js
+++ candy/static/candy/form.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('.post-form');
     form.addEventListener('submit', function(event){
+        event.preventDefault();
         const errorField = document.querySelector('.error');
         const allFields = document.querySelector('.input_custom');
 
@@ -8,15 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const weight = document.getElementById('id_weight').value;
         const price = document.getElementById('id_price').value;
         const rate = document.getElementById('id_rate').value;
-
+        
+        // Обработка ошибок //
+        let errorMessages = [];
         for (let i = 0; i < allFields.length; i++){
             if (!allFields[i].value) {
                 errorMessages.push('Заполните все поля!');
                 break;
             }
         }
-        
-        let errorMessages = [];
         if (name.length >= 50){
             errorMessages.push('Длина имени не должна превышать 50 символов!');
         }
@@ -31,5 +32,68 @@ document.addEventListener('DOMContentLoaded', () => {
             errorField.style.display = 'block';
             errorField.innerHTML = errorMessages.join('<br>\n'); 
         }
+        //////////////////////
+        // Если нет ошибок, отправить данные на сервер // 
+        if (errorMessages.length === 0){
+            let formData = {
+                name: name,
+                weight: weight,
+                price: price,
+                rate:rate,
+            };
+
+            let requestContent = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            };
+            fetch('/api/candies/', requestContent)
+                .then(response => {
+                    if (!response.ok) {
+                        return response.json().then(error => {
+                            throw new Error(error.detail || 'Что-то пошло не так');
+                        });
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('Успех:', data);
+                    window.location.href = 'https://3d20-91-238-229-3.ngrok-free.app';  // Перемещено сюда
+                })
+                .catch(error => {
+                    console.error('Ошибка:', error.message);
+                });
+        }
     })
 })
+
+
+
+// let formData = {
+//     name: 'name12121',
+//     weight: '111',
+//     price: '222',
+//     rate:'3',
+// };
+
+// let requestContent = {
+//     method: 'POST',
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify(formData)
+// };
+
+// fetch('/api/candies/', requestContent)
+//     .then(response => {
+//         if (!response.ok) {
+//             return response.json().then(error => {
+//                 throw new Error(error.detail || 'Что-то пошло не так');
+//             });
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log('Успех:', data);
+//     })
+//     .catch(error => {
+//         console.error('Ошибка:', error.message);
+//     });
